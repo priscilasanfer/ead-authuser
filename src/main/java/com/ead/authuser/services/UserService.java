44f14515a9b60ddec3c0afa1af2bc7601abe00f7
Priscila Ferreira <priscilasanfer@gmail.com>
2022-09-04 22:26:20 -0300

    Cria método de registro de usuário com verificações inicias

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -1,8 +1,6 @@
 package com.ead.authuser.services;
 
 import com.ead.authuser.models.UserModel;
-import org.springframework.http.ResponseEntity;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,4 +11,10 @@ public interface UserService {
     Optional<UserModel> findById(UUID userId);
 
     void delete(UserModel userModel);
+
+    void save(UserModel userModel);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
