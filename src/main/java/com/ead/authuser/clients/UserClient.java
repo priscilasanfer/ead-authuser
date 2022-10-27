b6f4af99fe783392e8f76b97422272a0b848aadd
Priscila Ferreira <priscilasanfer@gmail.com>
2022-10-27 20:12:28 -0300

    API Composition Pattern - Parte 01

---
+++ src/main/java/com/ead/authuser/clients/UserClient.java
@@ -0,0 +1,55 @@
+package com.ead.authuser.clients;
+
+import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Log4j2
+@Component
+public class UserClient {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    String REQUEST_URI = "http://localhost:8082";
+
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
+        List<CourseDto> searchResult = null;
+
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
+
+        String url = REQUEST_URI +
+                "/courses?userId=" + userId +
+                "&page=" + pageable.getPageNumber() +
+                "&size=" + pageable.getPageSize() +
+                "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+        log.debug("Request URL: {}", url);
+        log.info("Request URL: {}", url);
+
+        try {
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
+            log.debug("Response Number of elements: {}", searchResult.size());
+
+        } catch (HttpStatusCodeException e) {
+            log.error("Error Request /courses {}", e);
+        }
+
+        log.info("End of Request /course userId: {}", userId);
+
+        return result.getBody();
+    }
+}
